501cc1acdeb7ead977f7bc12ee14388da168b69c
Urban Rotnik <urban.rotnik@gmail.com>
2021-06-08 13:46:38 +0200

    User ENV for notion page ID

--- site.config.js
+++ site.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  // rootNotionPageId: '2abc0456603548a3884e6776e1cfc3d2',
+  rootNotionPageId: process.env.NOTION_PAGE_ID,
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Urban Rotnik',
+  domain: 'urbanrotnik.com',
+  author: 'Urban Rotnik',
 
   // open graph metadata (optional)
   description: 'Example site description',
