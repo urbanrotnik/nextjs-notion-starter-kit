7336cfec56d521fd25e64b078a57f69aae8ffb95
Urban Rotnik <urban.rotnik@gmail.com>
2021-10-12 11:21:11 +0200

    Update page ID

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '5513c7fbd64248818470ff396a7c40eb',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
